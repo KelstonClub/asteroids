65143543701b9542bd5ac4302736a0a94c291bbb
Tim Golden <mail@timgolden.me.uk>
2019-06-15 14:37:44 +0100

    Skeleton spaceship

---
+++ spaceship.py
@@ -0,0 +1,19 @@
+spaceship = Actor(...)
+
+def on_key_down(key):
+    if key == keys.left:
+        pass
+    elif key == keys.right:
+        pass
+    elif key == keys.space:
+        pass
+
+def on_key_up(key):
+    pass
+
+def update():
+    pass
+
+def draw():
+    pass
+
